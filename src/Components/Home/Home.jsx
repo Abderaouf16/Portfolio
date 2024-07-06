a00db0d0d10fb706ce8b7dd78de26f0bb5806ef8
Abderaouf16 <abderaoufa88@gmail.comm>
2024-07-06 15:59:33 +0100

    Feat: Edit education section 'delete see the place btn '

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -275,15 +275,15 @@ const App = () => {
       </Element>
       <Element name="Education">
         <section className="py-10 md:py-16">
-          <div className="container max-w-screen-xl mx-auto px-4">
+          <div className=" container max-w-screen-xl mx-auto px-4">
             <h1 className="font-medium text-gray-700 text-3xl md:text-4xl mb-5">
               Education
             </h1>
             <p className="font-normal text-gray-500 text-xs md:text-base mb-20">
               Below is a summary of the places I studied
             </p>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-              <div className="bg-gray-50 px-8 py-10 rounded-md">
+            <div className="  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              <div className="bg-gray-100 px-8 py-10 rounded-md">
                 <h4 className="font-medium text-gray-700 text-lg mb-1">
                   BSc in Architectural Engineering & Design
                 </h4>
@@ -299,14 +299,8 @@ const App = () => {
                   <br />I have developed strong visual skills, and a grasp of
                   mathematics, and the history of architecture.
                 </p>
-                <div className="relative">
-                  <h6 className="font-semibold text-gray-500 text-md relative z-10">
-                    See the place here
-                  </h6>
-                  <span className="w-32 h-1 bg-blue-200 absolute bottom-1 left-0 z-0"></span>
-                </div>
               </div>
-              <div className="bg-gray-50 px-8 py-10 rounded-md">
+              <div className="bg-gray-100 px-8 py-10 rounded-md">
                 <h4 className="font-medium text-gray-700 text-lg mb-1">
                   Meta Front-End Developer
                 </h4>
@@ -319,14 +313,8 @@ const App = () => {
                   UI/UX design, React Native development <br />
                   and web development in general.
                 </p>
-                <div className="relative">
-                  <h6 className="font-semibold text-gray-500 text-md relative z-10">
-                    See the place here
-                  </h6>
-                  <span className="w-32 h-1 bg-blue-200 absolute bottom-1 left-0 z-0"></span>
-                </div>
               </div>
-              <div className="bg-gray-50 px-8 py-10 rounded-md">
+              <div className="bg-gray-100 px-8 py-10 rounded-md">
                 <h4 className="font-medium text-gray-700 text-lg mb-1">
                   Web development Bootcamp
                 </h4>
@@ -341,12 +329,12 @@ const App = () => {
                   program focuses on self-directed technical training in coding
                   and web design as well as Capstone Project.
                 </p>
-                <div className="relative">
+                {/* <div className="relative">
                   <h6 className="font-semibold text-gray-500 text-md relative z-10">
                     See the place here
                   </h6>
                   <span className="w-32 h-1 bg-blue-200 absolute bottom-1 left-0 z-0"></span>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
